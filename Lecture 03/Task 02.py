3ae87bb1fc5ad8d758e4d717bb9c7f80cf113f3c
rabayathossain27-cell <rabayathossain27@gmail.com>
2025-09-16 00:34:00 +0300

    I have finished mt task02 from Lecture03

---
+++ Lecture 03/Task 02.py
@@ -0,0 +1,14 @@
+# Ask the user for the cabin class
+cabin_class = input("Enter the cabin class (LUX, A, B, C): ").strip().upper()
+
+# Check the class using if/elif/else
+if cabin_class == "LUX":
+    print("Upper-deck cabin with a balcony.")
+elif cabin_class == "A":
+    print("Cabin above the car deck with a window.")
+elif cabin_class == "B":
+    print("Windowless cabin above the car deck.")
+elif cabin_class == "C":
+    print("Windowless cabin below the car deck.")
+else:
+    print("Invalid cabin class")
